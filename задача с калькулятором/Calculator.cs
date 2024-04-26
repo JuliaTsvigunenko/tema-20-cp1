0c7651227c6eefd0d3817d0835e0af83c8c73e2d
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-04-26 14:01:14 +0300

    Add files via upload

---
+++ задача с калькулятором/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculator<T>
+    {
+       
+        public dynamic a;
+        public dynamic b;
+
+        /// <summary>
+        /// Метод для сложения
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="b1"></param>
+        /// <returns></returns>
+        public T Add(T a1, T b1)
+        {
+            dynamic a = a1;
+            dynamic b = b1;
+            return a + b;
+        }
+
+        /// <summary>
+        /// Метод для вычитания
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="b1"></param>
+        /// <returns></returns>
+        public T Sub(T a1, T b1)
+        {
+            dynamic a = a1;
+            dynamic b = b1;
+            return a - b;
+        }
+
+        /// <summary>
+        /// Метод для умножения
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="b1"></param>
+        /// <returns></returns>
+        public T Mul(T a1, T b1)
+        {
+            dynamic a = a1;
+            dynamic b = b1;
+            return a * b;
+        }
+
+        /// <summary>
+        /// Метод для деления
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="b1"></param>
+        /// <returns></returns>
+        public T Div(T a1, T b1)
+        {
+            dynamic a = a1;
+            dynamic b = b1;
+            return a/b;
+        }
+
+        /// <summary>
+        /// Метод, где на 0 делить нельзя
+        /// </summary>
+        /// <returns></returns>
+        public dynamic ShhowInfo()
+        {
+            if(b==0)
+            {
+                Console.WriteLine("Делить на ноль нельзя");
+            }
+            return a / b;
+        }
+
+
+
+    }
+}
---
+++ задача с калькулятором/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator<int> a = new Calculator<int>();
+            Console.WriteLine($"Сложение: {a.Add(18, 2)}");
+            Console.WriteLine($"Вычитание: {a.Sub(15, 10)}");
+            Console.WriteLine($"Умножение: {a.Mul(12, 12)}");
+            Console.WriteLine($"Деление: {a.Div(6, 2)}");
+
+            Calculator<double> v = new Calculator<double>();
+            Console.WriteLine($"Сложение: {v.Add(11.5, 13.5)}");
+            Console.WriteLine($"Вычитание: {v.Sub(15.1, 10.1)}");
+            Console.WriteLine($"Умножение: { v.Mul(1.2,2.3)}");
+            Console.WriteLine($"Вычитание: {v.Div(6.4, 2.4)}");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
